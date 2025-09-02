d064e8ae92fca59d466560b064763550bd3018d5
Alexis La Torre <alexis-latorre@live.fr>
2025-09-02 14:51:04 +0200

    P3C1

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,4 @@
-# Ecrivez votre code ici !
+import operations
+
+print(f"Le résultat de l'addition de 3 et 5 est {operations.addition(3, 5)}")
+print(f"Le résultat de la multiplication de 8 et 2 est {operations.multiplication(8, 2)}")
--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+def controler_nombres(nombres):
+    for nombre in nombres:
+        if type(nombre) == str and not str.isnumeric(nombre):
+            print(f"Le nombre '{nombre}' est incorrect.")
+            return False
+    return True
+
+def addition(a, b):
+    if controler_nombres([a, b]):
+        return round(float(a) + float(b))
+        
+def multiplication(a, b):
+    if controler_nombres([a, b]):
+        return round(float(a) * float(b))
